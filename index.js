a5e598eb236f18ed93b60ad0c91be72da19b1a39
yumeyuniko <daigocafe@msn.com>
2022-03-09 20:04:59 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const PORT = 5005;
+
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const app = express();
+
+//scraping
+const URL = 'https://search.rakuten.co.jp/search/mall/keyboard/';
+const data = [];
+
+axios(URL)
+  .then((response) => {
+    const htmlParser = response.data;
+    // console.log(htmlParser);
+
+    const $ = cheerio.load(htmlParser);
+
+    $('.searchresultitem', htmlParser).each(function () {
+      const title = $(this).find('.title').text();
+      const price = $(this).find('.important').text();
+
+      data.push({ title, price });
+      console.log(data);
+    });
+  })
+  .catch((e) => console.log(e));
+
+app.listen(PORT, console.log('server running'));
